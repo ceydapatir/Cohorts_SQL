8647ccc317fc057fc12a17f7751b23be8d7046ec
Ceyda Patır <56155958+ceydapatir@users.noreply.github.com>
2023-10-06 15:59:32 +0300

    Add files via upload

---
+++ ödev10.sql
@@ -0,0 +1,14 @@
+** City tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+
+SELECT city, country FROM city
+LEFT JOIN country ON country.country_id = city.country_id;
+
+** Customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+
+SELECT payment_id, first_name, last_name FROM customer
+RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
+
+** Customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
+SELECT rental_id, first_name, last_name FROM customer
+RIGHT JOIN rental ON rental.customer_id = customer.customer_id;
